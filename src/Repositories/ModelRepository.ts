36bf69bc212f0b0e3c85874e878d082227065330
Lee Brooks <leebrooks93@gmail.com>
2018-09-10 20:36:19 +0100

    Add base repository

--- src/Repositories/ModelRepository.ts
+++ src/Repositories/ModelRepository.ts
@@ -2,36 +2,9 @@ import ClientInterface from '../Http/Clients/ClientInterface';
 import ResponseInterface from '../Http/Responses/ResponseInterface';
 import ModelInterface from '../Models/ModelInterface';
 import ModelRepositoryInterface from './ModelRepositoryInterface';
+import Repository from './Repository';
 
-export default abstract class ModelRepository<Model> implements ModelRepositoryInterface<Model> {
-    /**
-     * A client instance.
-     *
-     * @var {ClientInterface}
-     */
-    protected client: ClientInterface;
-
-    /**
-     * The restful API endpoint for this model.
-     *
-     * @var {string}
-     */
-    protected endpoint: string;
-
-    /**
-     * The namespace or name of the key used to wrap the main data of the payload.
-     *
-     * @var {string}
-     */
-    protected namespace?: string = 'data';
-
-    /**
-     * @param {ClientInterface} client
-     */
-    public constructor(client: ClientInterface) {
-        this.client = client;
-    }
-
+export default abstract class ModelRepository<Model> extends Repository<Model> implements ModelRepositoryInterface<Model> { // tslint:disable-line max-line-length
     /** @inheritDoc */
     public async get(params: object = {}): Promise<Model[]> {
         const client: ClientInterface = this.getClient();
@@ -82,51 +55,4 @@ export default abstract class ModelRepository<Model> implements ModelRepositoryI
 
         await client.delete(`${endpoint}/${id}`, params);
     }
-
-    /**
-     * Returns the client.
-     *
-     * @return {ClientInterface}
-     */
-    protected getClient(): ClientInterface {
-        return this.client;
-    }
-
-    /**
-     * Returns the endpoint.
-     *
-     * @return {string}
-     */
-    protected getEndpoint(): string {
-        return this.endpoint;
-    }
-
-    /**
-     * Returns the namespace.
-     *
-     * @return {string|null}
-     */
-    protected getNamespace(): string | null {
-        return this.namespace === undefined ? null : this.namespace;
-    }
-
-    /**
-     * Returns the unwrapped main data from the response payload.
-     *
-     * @param {ResponseInterface} response
-     * @return {*}
-     */
-    protected getResponseData(response: ResponseInterface): any {
-        const namespace: string | null = this.getNamespace();
-
-        return namespace ? response.data[namespace] : response.data;
-    }
-
-    /**
-     * Makes an instance of the model.
-     *
-     * @param {object} attributes
-     * @return {ModelInterface}
-     */
-    protected abstract makeModel(attributes: object): Model;
 }
---
+++ src/Repositories/Repository.ts
@@ -0,0 +1,80 @@
+import ClientInterface from '../Http/Clients/ClientInterface';
+import ResponseInterface from '../Http/Responses/ResponseInterface';
+import ModelInterface from '../Models/ModelInterface';
+
+export default abstract class Repository<Model> {
+    /**
+     * A client instance.
+     *
+     * @var {ClientInterface}
+     */
+    protected client: ClientInterface;
+
+    /**
+     * The restful API endpoint for this model.
+     *
+     * @var {string}
+     */
+    protected endpoint: string;
+
+    /**
+     * The namespace or name of the key used to wrap the main data of the payload.
+     *
+     * @var {string}
+     */
+    protected namespace?: string = 'data';
+
+    /**
+     * @param {ClientInterface} client
+     */
+    public constructor(client: ClientInterface) {
+        this.client = client;
+    }
+
+    /**
+     * Returns the client.
+     *
+     * @return {ClientInterface}
+     */
+    protected getClient(): ClientInterface {
+        return this.client;
+    }
+
+    /**
+     * Returns the endpoint.
+     *
+     * @return {string}
+     */
+    protected getEndpoint(): string {
+        return this.endpoint;
+    }
+
+    /**
+     * Returns the namespace.
+     *
+     * @return {string|null}
+     */
+    protected getNamespace(): string | null {
+        return this.namespace === undefined ? null : this.namespace;
+    }
+
+    /**
+     * Returns the unwrapped main data from the response payload.
+     *
+     * @param {ResponseInterface} response
+     * @return {*}
+     */
+    protected getResponseData(response: ResponseInterface): any {
+        const namespace: string | null = this.getNamespace();
+
+        return namespace ? response.data[namespace] : response.data;
+    }
+
+    /**
+     * Makes an instance of the model.
+     *
+     * @param {object} attributes
+     * @return {ModelInterface}
+     */
+    protected abstract makeModel(attributes: object): Model;
+}
