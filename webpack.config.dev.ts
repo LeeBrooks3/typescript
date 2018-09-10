5be8cf9e71fd4f52315ae18480d13582a5bf6eab
Lee Brooks <leebrooks93@gmail.com>
2018-09-10 09:13:13 +0100

    Reconfigure build

--- webpack.config.dev.ts
+++
@@ -1,7 +0,0 @@
-import config from './webpack.config';
-
-const BundleAnalyzerPlugin: any = require('webpack-bundle-analyzer').BundleAnalyzerPlugin; // tslint:disable-line no-var-requires max-line-length
-
-config.plugins.push(new BundleAnalyzerPlugin());
-
-export default config;
