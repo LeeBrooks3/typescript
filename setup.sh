15e327673f4995e0940e3ab19d209c7fb4e224dc
Lee Brooks <leebrooks93@gmail.com>
2018-09-13 19:57:59 +0100

    Try pre-commit hook

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # set up githook to build pre-push
-echo "npm run build" >> ./.git/hooks/pre-push
+echo "npm run build" >> ./.git/hooks/pre-commit
 
 # install node modules and build
 npm install && npm run build
